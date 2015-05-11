b68852d8839a515be50d676e212071ed9f122e5c
Luiz Branco <me@luizbranco.com>
2015-05-10 21:55:29 -0400

    Write header to connection

--- eventsource.go
+++ eventsource.go
@@ -1,24 +1,21 @@
 package eventsource
 
 import (
+  "fmt"
   "net/http"
 )
 
-var retry []byte = []byte("retry: 2000\n")
+var header string = `HTTP/1.1 200 OK
+Content-Type: text/event-stream
+Cache-Control: no-cache
+Connection: keep-alive
+Access-Control-Allow-Origin: %s
+Access-Control-Allow-Credentials: true
 
-func setHeader (res http.ResponseWriter, req *http.Request) {
-  origin := req.Header.Get("origin")
-  header := res.Header()
-  header.Set("Access-Control-Allow-Origin", origin)
-  header.Set("Access-Control-Allow-Credentials", "true")
-  header.Set("Content-Type", "text/event-stream")
-  header.Set("Cache-Control", "no-cache")
-  header.Set("Connection", "keep-alive")
-}
+retry: 2000
 
+`
 func Handler (res http.ResponseWriter, req *http.Request) {
-  setHeader(res, req)
-
   hj, ok := res.(http.Hijacker)
   if !ok {
     http.Error(res, "webserver doesn't support hijacking", http.StatusInternalServerError)
@@ -31,7 +28,9 @@ func Handler (res http.ResponseWriter, req *http.Request) {
     return
   }
 
-  _, err = conn.Write(retry)
+  origin := req.Header.Get("origin")
+  h := fmt.Sprintf(header, origin)
+  _, err = conn.Write([]byte(h))
 
   if err != nil {
     conn.Close()
