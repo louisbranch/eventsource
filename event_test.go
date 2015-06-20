444e72b92a4b980e2c6fd51826e05e122e378918
Luiz Branco <me@luizbranco.com>
2015-06-20 13:46:40 -0400

    Add initial test for send method

--- event_test.go
+++ event_test.go
@@ -8,6 +8,20 @@ import (
 var message []byte = []byte("{id: 1}")
 var deflated = "eJyqzkyxUjCsBQQAAP//CfUCUQ=="
 
+func TestEventSendToClient(t *testing.T) {
+	e := Event{Message: message}
+	c := client{events: make(chan payload)}
+	go e.send([]client{c})
+	p := <-c.events
+
+	expecting := e.bytes()
+	result := p.data
+
+	if !bytes.Equal(expecting, result) {
+		t.Errorf("expected:\n%s\ngot:\n%s\n", expecting, result)
+	}
+}
+
 func TestEventBytesWithName(t *testing.T) {
 	expecting := []byte("event: test\ndata: {id: 1}\n\n")
 	e := Event{
